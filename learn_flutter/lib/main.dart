2a2e91c00ff51c982d94fecc8b0a8e54ce357375
tangyunchuan <tangyunchuan@yuanxin-inc.com>
2020-08-04 14:22:16 +0800

    方法的组件化

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -7,27 +7,53 @@ import 'package:flutter/material.dart';
  */
 
 ///main函数作为入口
-void main() {
-  runApp(
-    MaterialApp(
+//void main() {
+//  runApp(
+//    MyApp()
+//  );
+//}
+
+main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
       title: "hello world abc",//Android 多应用切换时会显示的标题
       home: Scaffold(
-        appBar: AppBar(
-          title: Text(
-              "我是标题"
-          ),
-        ),
-        body: Center(
-          child: Text(
-            "hello world",
-            textDirection: TextDirection.ltr,
-            style: TextStyle(
-                fontSize: 30,
-                color: Colors.orange
+          appBar: AppBar(
+            title: Text(
+                "我是标题"
             ),
           ),
-        ),
+          body: ContentWidget()
       ),
-    )
-  );
+    );
+  }
+}
+
+class ContentWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: TextWidget()
+    );
+  }
+}
+
+class TextWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Text(
+      "hello world",
+      textDirection: TextDirection.ltr,
+      style: TextStyle(
+          fontSize: 30,
+          color: Colors.orange
+      ),
+    );
+  }
 }
