d659e92a928babd77a8e8f6d33392fe11994833b
tangyunchuan <tangyunchuan@yuanxin-inc.com>
2020-08-05 11:08:20 +0800

    按钮及自定义按钮

--- learn_flutter/lib/main.dart
+++ learn_flutter/lib/main.dart
@@ -23,25 +23,74 @@ class ContentWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    return CustomButtonDemo();
+  }
+}
+
+
+class CustomButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+
+      child: RaisedButton(
+        color: Colors.red,
+        onPressed: () => print("RaisedButton点击"),
+        child: Row(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Icon(Icons.people, color: Colors.white),
+            SizedBox(width: 10),
+            Text(
+              "自定义按钮",
+              style: TextStyle(color: Colors.white, fontSize: 20))
+          ],
+        ),
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8))
+      ),
+    );
+  }
+}
+
+class ButtonDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Column(
+      children: <Widget>[
+        RaisedButton(onPressed: () => print("RaisedButton被电击了"), child: Text("RaisedButton")),
+        FlatButton(onPressed: () => print("FlatButton被电击了"), child: Text("FlatButton")),
+        FloatingActionButton(onPressed: () => print("FloatingActionButton被电击了"), child: Text("FloatingActionButton")),
+        OutlineButton(onPressed: () => print("RawMaterialButton被电击了"), child: Text("RawMaterialButton")),
+      ],
+    );
+  }
+}
+
+class TextRichDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
     return Text.rich(
       TextSpan(
-        children: [
-          TextSpan(
-              text: "哈哈哈哈",
-              style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold
-              )
-          ),
-          TextSpan(
-            text: "舒适",
-            style: TextStyle(fontSize: 18)
-          ),
-          TextSpan(
-           text: "\n的发售的福利康师傅时间肯定会恐惧阿贾克斯把v数据库的v北京",
-            style: TextStyle(color: Colors.pink)
-          )
-        ]
+          children: [
+            TextSpan(
+                text: "哈哈哈哈",
+                style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold
+                )
+            ),
+            TextSpan(
+                text: "舒适",
+                style: TextStyle(fontSize: 18)
+            ),
+            TextSpan(
+                text: "\n的发售的福利康师傅时间肯定会恐惧阿贾克斯把v数据库的v北京",
+                style: TextStyle(color: Colors.pink)
+            )
+          ]
       ),
       textAlign: TextAlign.center,
     );
